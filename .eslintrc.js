053e63b0fdc96fc2569d048ed24cf941372a32f4
Joel Alvarez Mexia <139817810+jeresoftx@users.noreply.github.com>
2023-08-25 09:00:44 -0700

    chore(typescript): :technologist: add typescript configuration files
    closed #52

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,36 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
   },
   plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
   rules: {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'prettier/prettier': 'error',
+    'operator-linebreak': ['error', 'before'],
   },
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
   ignorePatterns: ['.eslintrc.js', 'commitlint.config.js', 'jest.config.js'],
 };
