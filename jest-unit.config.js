7ce5bd8dbf93b608824e899e4c612ac9488376ea
Joel Alvarez Mexia <139817810+jeresoftx@users.noreply.github.com>
2023-08-25 08:23:48 -0700

    test: :technologist: add jest configuration files
    closed #10

---
+++ jest-unit.config.js
@@ -0,0 +1,15 @@
+const { resolve } = require('path');
+
+const root = resolve(__dirname);
+
+module.exports = {
+  rootDir: root,
+  displayName: 'test',
+  testRegex: '/tests/units/.*\\.test\\.(js|ts)$',
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '^mongoose$': '<rootDir>/node_modules/mongoose',
+  },
+  // setupFilesAfterEnv: ['<rootDir>/tests/unit/bootstrap.js'],
+};
---
+++ tests/units/verion/version.test.ts
@@ -0,0 +1,5 @@
+describe('Graphql version', () => {
+  it('Should return the graphql current version', async () => {
+    expect('test').toBe('test');
+  });
+});
