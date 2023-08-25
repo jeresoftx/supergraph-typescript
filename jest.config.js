9c5d46ab48679b66ccea35ce6f55b6d46eb43c8e
Joel Alvarez Mexia <139817810+jeresoftx@users.noreply.github.com>
2023-08-25 08:27:16 -0700

    test: :technologist: add jest configuration files (#49)
    closed #10

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+const { resolve } = require('path');
+
+const root = resolve(__dirname);
+
+module.exports = {
+  rootDir: root,
+  preset: 'ts-jest',
+  projects: [
+    '<rootDir>/jest-unit.config.js',
+    '<rootDir>/jest-integration.config.js',
+  ],
+  coverageDirectory: '<rootDir>/coverage/',
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'html'],
+  testEnvironment: 'node',
+  verbose: true,
+  cacheDirectory: '.tmp/jestCache',
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+  collectCoverageFrom: ['src/**/*.js', '!src/server/*.js', '!src/index.js'],
+};
